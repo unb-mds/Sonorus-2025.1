750622f5b53bad369282221b360725a078e537d5
rafaelbdmelo117 <rafael_b_de_melo@hotmail.com>
2025-05-17 16:11:58 -0300

    Adiciona algumas mudanças no css e no jsx do LoginRegistro

--- src/frontend/src/Components/LoginRegistro/LoginRegistro.jsx
+++ src/frontend/src/Components/LoginRegistro/LoginRegistro.jsx
@@ -3,31 +3,21 @@ import './LoginRegistro.css';
 
 const LoginRegistro = () => {
     return (
-        <div className='Wrapper'>
+        <div className='BlocoPreto'>
             <div className='form-box Entrar'>
                 <form action=''>
                     <h1>Entrar</h1>
                         <div className="input-box">
-                            <input type="text" placeholder='Usuário' required />
+                            <input type="text" placeholder='Email' required />
                         </div>
                         <div className="input-box">
                             <input type="password" placeholder='Senha' required />
                         </div>
 
-                        <div className="remember-forgot">
-                            <label>
-                                <input type="checkbox"/>
-                                Lembre de mim
-                            </label>
+                        <div className="forgot">
                             <a href="#">Esqueceu sua senha?</a>
                         </div>
-                        <button type="submit" className='btn'>Login</button>
-
-                        <div className="register-link">
-                            <p>Primeira vez? Faça seu cadastro! <a href="#">
-                                <button type="submit" className='btn'>Cadastre-se</button></a>
-                            </p>
-                        </div>
+                        <button type="submit" className='btn'>LOGIN</button>
                 </form>
             </div>
         </div>

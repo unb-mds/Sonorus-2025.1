efa0bac2a48d3dc6464997695cc517a0a962a4b4
Dodeglinhass <wdouglas939@gmail.com>
2025-05-29 10:30:18 -0300

    Resolve conflitos com main

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,22 +1,18 @@
+# src/backend/main.py
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 from backend.database.db_connection import SessionLocal  # Importação atualizada!
-from src.backend.database.db_connection import SessionLocal
-from src.backend.api.endpoints_voz import roteador_autenticacao as wav_router
-from src.backend.api.autenticacao import roteador_autenticacao as autenticacao_router
-from src.backend.api.endpoints_banco import roteador_banco
 
 app = FastAPI()
 
-app.include_router(autenticacao_router)
-
-app.include_router(wav_router)
-
-app.include_router(roteador_banco)
-
+# Função para obter a sessão do banco
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
+
+@app.get("/test-db")
+async def test_db(db: Session = Depends(get_db)):
+    return {"status": "Conectado ao PostgreSQL!"}

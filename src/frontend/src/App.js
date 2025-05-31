ce1c915b1ac47f8bb8499b71c18432e2c433eda5
rafaelbdmelo117 <rafael_b_de_melo@hotmail.com>
2025-05-31 13:05:26 -0300

    Adiciona tela de erro de cadastro

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './Components/Login/Login';
 import Register from './Components/Registro/Register';
+import ErroCadastro from './Components/TelaErro/ErroCadastro';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
           <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/erroCadastro" element={<ErroCadastro />} />
         </Routes>
       </div>
     </Router>

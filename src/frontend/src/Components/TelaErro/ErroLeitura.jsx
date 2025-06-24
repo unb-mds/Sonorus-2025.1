912874e5bcfd31f383ba848edd8f2ae565d414a1
Pauloswimming <paulocontaoficialph@gmail.com>
2025-06-24 17:15:23 -0300

    implementa testes unitários para o componente Login

--- src/frontend/src/Components/TelaErro/ErroLeitura.jsx
+++ src/frontend/src/Components/TelaErro/ErroLeitura.jsx
@@ -1,18 +1,29 @@
-import { useState } from 'react';
+import React from 'react';
 import './ErroLeitura.css';
-
+import { useNavigate } from 'react-router-dom';
 
 const ErroLeitura = () => {
+  const navigate = useNavigate();
+
+  const handleVoltarLogin = () => {
+    navigate('/login');
+  };
+
   return (
-      <div className="erro-card">
-        <h1 className="erro-title">Algo deu errado.</h1>
-        <div className="erro-icon">
-          <div className="circle">
-            <span className="cruz">×</span>
-          </div>
+    <div className="erro-card">
+      <h1 className="erro-title">Algo deu errado.</h1>
+      <div className="erro-icon">
+        <div className="circle">
+          <span className="cruz">×</span>
         </div>
-        <p className="erro-message">Vamos tentar novamente.</p>
       </div>
+      <button
+        className="botao-voltar-login"
+        onClick={handleVoltarLogin}
+      >
+        Voltar para o Login
+      </button>
+    </div>
   );
 };
 
